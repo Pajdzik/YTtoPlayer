4a6cb3d7074049c18b1641eb0077bab94b8343c5
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-02-19 13:33:49 +0100

    Add DefaultBootstrapper class.

---
+++ YTtoPlayer/YTtoPlayer.Core/ViewModels/Configuration/DefaultBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace YTtoPlayer.Core.ViewModels.Configuration
+{
+    using System.Windows;
+
+    using Caliburn.Micro;
+
+    internal class DefaultBootstrapper : BootstrapperBase
+    {
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            this.DisplayRootViewFor<MainViewModel>();
+            base.OnStartup(sender, e);
+        }
+    }
+}
