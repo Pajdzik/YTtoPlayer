dc86ff4ccf0926d6e0edc18804b94337151b7f77
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-02-22 16:13:30 +0100

    Add VideoInfoViewModel.

---
+++ YTtoPlayer/YTtoPlayer.Core/ViewModels/VideoInfoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+
+namespace YTtoPlayer.Core.ViewModels
+{
+    using Google.Apis.YouTube.v3.Data;
+
+    public class VideoInfoViewModel
+    {
+        private readonly string videoUrl;
+
+        public VideoInfoViewModel(string videoUrl)
+        {
+            this.videoUrl = videoUrl;
+            this.LoadInfo();
+        }
+
+        private void LoadInfo()
+        {
+            
+        }
+    }
+}
