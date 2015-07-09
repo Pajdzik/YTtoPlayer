b6904df108833541940bc565af6040a90ed501dd
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-07-09 14:20:46 +0200

    Add ILocationPicker and WpfLocationPicker.

---
+++ YTtoPlayer/YTtoPlayer.Core/Models/ILocationPicker.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace YTtoPlayer.Core.Models
+{
+    public interface ILocationPicker
+    {
+        string ChooseLocation();
+    }
+}
---
+++ YTtoPlayer/YTtoPlayer.View/Helpers/WpfLocationPicker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using YTtoPlayer.Core.Models;
+
+namespace YTtoPlayer.View.Helpers
+{
+    public class WpfLocationPicker : ILocationPicker
+    {
+        public string ChooseLocation()
+        {
+            var folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+            string selectedPath = folderBrowserDialog.SelectedPath;
+
+            return selectedPath;
+        }
+    }
+}
