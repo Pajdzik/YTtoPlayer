9c49763277f8124606579a263ba94146c3f03074
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-07-09 14:21:33 +0200

    Add YouTubeVideoInfoViewModel.

---
+++ YTtoPlayer/YTtoPlayer.Core/ViewModels/YouTubeVideoInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using PropertyChanged;
+using YTtoPlayer.Core.Models;
+
+namespace YTtoPlayer.Core.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class YouTubeVideoInfoViewModel : ViewModelBase
+    {
+        private readonly MainViewModel mainViewModel;
+
+        public YouTubeVideoInfoViewModel(MainViewModel mainViewModel, IList<YouTubeVideoInfo> youTubeVideoInfos)
+        {
+            this.mainViewModel = mainViewModel;
+            this.YouTubeVideoInfos = new ObservableCollection<YouTubeVideoInfo>(youTubeVideoInfos);
+        }
+
+        public YouTubeVideoInfo SelectedYouTubeVideoInfo { get; set; }
+
+        public ObservableCollection<YouTubeVideoInfo> YouTubeVideoInfos { get; set; }
+    }
+}
