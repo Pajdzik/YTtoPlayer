95fc3f8ae9779305bc532c583fd4214448207a41
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-07-09 14:23:09 +0200

    Add MainView.

--- YTtoPlayer/YTtoPlayer.View/Views/MainView.xaml.cs
+++ YTtoPlayer/YTtoPlayer.View/Views/MainView.xaml.cs
@@ -1,4 +1,13 @@
-﻿namespace YTtoPlayer.View.Views
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using Newtonsoft.Json;
+using YTtoPlayer.Core.Resources;
+using YTtoPlayer.Core.ViewModels;
+using YTtoPlayer.View.Helpers;
+
+namespace YTtoPlayer.View.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +17,21 @@
         public MainView()
         {
             InitializeComponent();
+            Settings settings = this.LoadSettings();
+            this.DataContext = new MainViewModel(settings, new WpfLocationPicker());
+        }
+
+        private Settings LoadSettings()
+        {
+            string baseDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string filePath = Path.Combine(baseDirectory, "ytconfig.json");
+
+            using (var streamReader = new StreamReader(filePath))
+            {
+                string settingsJson = streamReader.ReadToEnd();
+                Settings settings = JsonConvert.DeserializeObject<Settings>(settingsJson);
+                return settings;
+            }
         }
     }
 }
