587d74fe49b9c916e357a5507d12c1825a05e2cf
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-07-09 14:22:26 +0200

    Implement MainViewModel.

--- YTtoPlayer/YTtoPlayer.Core/ViewModels/MainViewModel.cs
+++ YTtoPlayer/YTtoPlayer.Core/ViewModels/MainViewModel.cs
@@ -1,5 +1,30 @@
-using Caliburn.Micro;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using PropertyChanged;
+using YTtoPlayer.Core.Models;
+using YTtoPlayer.Core.Resources;
 
 namespace YTtoPlayer.Core.ViewModels {
-    public class MainViewModel : PropertyChangedBase { }
+
+    [ImplementPropertyChanged]
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel(Settings settings, ILocationPicker locationPicker)
+        {
+            this.Settings = settings;
+            this.YouTubeUrlViewModel = new YouTubeUrlViewModel(this);
+            this.FileLocationViewModel = new FileLocationViewModel(this, locationPicker);
+        }
+
+        public YouTubeUrlViewModel YouTubeUrlViewModel { get; set; }
+
+        public YouTubeVideoInfoViewModel YouTubeVideoInfoViewModel { get; set; }
+
+        public FileLocationViewModel FileLocationViewModel { get; set; }
+
+        public int WizardStep { get; set; }
+
+        public Settings Settings { get; private set; }
+    }
 }
