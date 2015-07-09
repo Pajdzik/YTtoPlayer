5c838807b8f54dcc447a5f52c5c33cfedd270199
Kamil Pajdzik <pajdziupaj@gmail.com>
2015-07-09 14:22:37 +0200

    Implement YouTubeUrlViewModel.

--- YTtoPlayer/YTtoPlayer.Core/Models/YouTubeVideoInfo.cs
+++ YTtoPlayer/YTtoPlayer.Core/Models/YouTubeVideoInfo.cs
@@ -1,13 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace YTtoPlayer.Core.Models
 {
-    class YouTubeVideoInfo
+    public class YouTubeVideoInfo
     {
+        private const string YouTubeThumbnailUrl = @"https://i.ytimg.com/vi/{0}/default.jpg";
+
+        private const string IdRegexPattern = @"^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\""'>]+)";
+
+        private readonly Regex idRegex = new Regex(IdRegexPattern);
+
+        private string id;
+
+        public YouTubeVideoInfo()
+        {
+        }
+
+        public YouTubeVideoInfo(Uri videoUri)
+        {
+            VideoUri = videoUri;
+        }
+
+        public Uri VideoUri { get; set; }
+
         public int ThumbnailHeight { get; set; }
 
         public string Type { get; set; }
@@ -33,5 +49,30 @@ namespace YTtoPlayer.Core.Models
         public string ProviderName { get; set; }
 
         public int Height { get; set; }
+
+        public string Id
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.id))
+                {
+                    this.id = this.GetId(this.VideoUri.AbsoluteUri);
+                }
+
+                return id;
+            }
+
+            set { id = value; }
+        }
+
+        public string ImagePath => string.Format(YouTubeThumbnailUrl, this.Id);
+
+        private string GetId(string videoUrl)
+        {
+            Match idMatch = this.idRegex.Match(videoUrl);
+            Group idGroup = idMatch.Groups[1];
+            return idGroup.Value;
+        }
+
     }
 }
